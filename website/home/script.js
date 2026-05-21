a35fa5abb39c312a581b9c18b20e410e049eb64f
durgaprasadkarri <durgaprasadkarri.cc@gmail.com>
2026-05-21 10:01:26 +0530

    README

--- website/home/script.js
+++ website/home/script.js
@@ -1,31 +1,15 @@
 const darkBtn = document.getElementById("darkModeToggle");
 
-// LOAD saved theme when page opens
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  if (darkBtn) {
+darkBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  // Change icon
+  if(document.body.classList.contains("dark-mode")){
     darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
   }
-} else {
-  if (darkBtn) {
+  else{
     darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
   }
-}
 
-// TOGGLE BUTTON
-if (darkBtn) {
-  darkBtn.addEventListener("click", () => {
-
-    document.body.classList.toggle("dark-mode");
-
-    // SAVE theme globally
-    if (document.body.classList.contains("dark-mode")) {
-      localStorage.setItem("theme", "dark");
-      darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    } else {
-      localStorage.setItem("theme", "light");
-      darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
-    }
-
-  });
-}
+});
