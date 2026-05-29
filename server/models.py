370d0ef457fa8a52fb37a1ce1be05679cddcbf79
adithya782 <srisaiadithya782@gmail.com>
2026-05-29 22:48:24 +0530

    Minor change in database

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key = True, autoincrement= True)
     fullname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(300), nullable = False)
+    email = db.Column(db.String(300), nullable = False, unique=True)
     password = db.Column(db.String(300), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
     google_id = db.Column(db.String(255), unique=True, nullable=True)
