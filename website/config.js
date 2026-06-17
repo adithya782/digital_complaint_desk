051fb69a850a0169ff3b8d7f6266584dfdf93077
adithya782 <srisaiadithya782@gmail.com>
2026-06-17 19:29:48 +0530

    Used gunicorn for switch to production server

--- website/config.js
+++ website/config.js
@@ -1,9 +1,10 @@
 // config.js
 const IS_PRODUCTION = true; // 🔄 Flip this to true when deploying!
 
-const API_BASE_URL = IS_PRODUCTION 
-    ? 'https://appear-eastbound-usable.ngrok-free.dev'      // Your live backend domain (Supabase/Render/AWS)
-    : 'http://127.0.0.1:5000';        // Your local Flask server
+const API_BASE_URL = IS_PRODUCTION
+  ? // ? 'https://appear-eastbound-usable.ngrok-free.dev'      // Your live backend domain (Supabase/Render/AWS)
+    "https://digital-complaint-desk-server.onrender.com" // Your live backend domain (Supabase/Render/AWS)
+  : "http://127.0.0.1:5000"; // Your local Flask server
 
 // Make it globally accessible across your scripts
 window.API_BASE_URL = API_BASE_URL;
